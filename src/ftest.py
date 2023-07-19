b7384b1f46692b389c314967ed1040f084287814
Arthur Feeney <afeeney@uci.edu>
2023-07-18 19:00:55 -0700

    Remove unused ftest script

--- src/ftest.py
+++
@@ -1,9 +0,0 @@
-from op_lib.metrics import fourier
-from op_lib.hdf5_dataset import TempDataset
-
-d = TempDataset('/data/homezvol2/afeeney/crsp/ai4ts/share/PB_simulation/SubCooled-FC72-2D_HDF5/Twall-100.hdf5',
-                time_window=3)
-item, label = d[0]
-
-
-fourier(item[0].unsqueeze(0), label, 8, 8)
