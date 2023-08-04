a40e5f7acead8e7378f29aed1c4f72cf593a75eb
Arthur Feeney <afeeney@uci.edu>
2023-08-04 01:39:24 -0700

    Reorganizations: 1. Initial setup for higher temporal resolution datasets 2. cleanup some of the model selection. Make more    settings customizable

---
+++ src/models/get_model.py
@@ -0,0 +1,55 @@
+from neuralop.models import FNO, UNO
+from .unet import UNet2d 
+from .twod_unet.twod_unet import Unet
+
+_UNET2D = 'unet2d'
+_UNET_MOD_ATTN = 'unet_mod_attn'
+
+_FNO = 'fno'
+
+_UNO = 'uno'
+
+_MODEL_LIST = [
+    _UNET2D,
+    _UNET_MOD_ATTN,
+    _FNO,
+    _UNO,
+]
+
+def get_model(model_name, in_channels, out_channels, exp):
+    assert model_name in _MODEL_LIST, f'Model name {model_name} invalid'
+    if model_name == _UNET_MOD_ATTN:
+        model = Unet(1, 1, 1, 0,
+                     time_history=exp.train.time_window,
+                     time_future=exp.train.future_window,
+                     hidden_channels=exp.model.hidden_channels,
+                     activation='gelu',
+                     mid_attn=True,
+                     norm=True,
+                     use1x1=True)
+    elif model_name == _UNET2D: 
+        model = UNet2d(in_channels=in_channels,
+                       out_channels=out_channels,
+                       init_features=exp.model.init_features)
+    elif model_name == _FNO:
+        model = FNO(n_modes=exp.model.n_modes,
+                    hidden_channels=exp.model.hidden_channels,
+                    domain_padding=exp.model.domain_padding,
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    n_layers=exp.model.n_layers,
+                    factorization='tucker',
+                    implementation='factorized',
+                    rank=0.05)
+    elif model_name == _UNO:
+        model = UNO(in_channels=in_channels, 
+                    out_channels=out_channels,
+                    hidden_channels=exp.model.hidden_channels,
+                    projection_channels=exp.model.projection_channels,
+                    uno_out_channels=[32,64,64,64,32],
+                    uno_n_modes=[[32,32],[16,16],[16,16],[16,16],[32,32]],
+                    uno_scalings=[[1,1],[0.5,0.5],[1,1],[1,1],[2,2]],
+                    n_layers=exp.model.n_layers,
+                    domain_padding=exp.model.domain_padding)
+    model = model.cuda().float()
+    return model
--- submit/basic.sh
+++ submit/basic.sh
@@ -13,11 +13,11 @@ conda activate bubble-sciml
 module load gcc/11.2.0
 
 python src/train.py \
-	data_base_dir=/share/crsp/lab/ai4ts/share/BubbleML/ \
+	data_base_dir=/share/crsp/lab/ai4ts/share/simul_ts_0.1/ \
 	log_dir=/share/crsp/lab/ai4ts/afeeney/log_dir \
-	dataset=PB_SubCooled \
+	dataset=PB_SubCooled_0.1 \
 	experiment=temp_unet2d \
-	experiment.train.max_epochs=1 \
+	experiment.train.max_epochs=2 \
 	#experiment.lr_scheduler.patience=50
 	#model_checkpoint=/share/crsp/lab/ai4ts/afeeney/log_dir/23089030/subcooled/UNet2d_vel_dataset_100_1691046606.pt \
 	#model_checkpoint=/data/homezvol2/afeeney/crsp/ai4ts/afeeney/thermal_models/subcooled/UNet2d_temp_input_dataset_500_1690005305.pt \
