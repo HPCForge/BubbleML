4cc9ef82e2eeb22107d00d86f4967a160f76d7aa
Arthur Feeney <afeeney@uci.edu>
2023-08-11 18:23:55 -0700

    Experiments setup for velocity predictions 1. configuration for model settings 2. push-forward trick implemented 3. plotting using jet colormap 4. train dataset reads from hdf5 files, rather than    reading from memory 5. validation uses in-memory dataset

--- scripts/boxkit_dataset.py
+++ scripts/boxkit_dataset.py
@@ -23,14 +23,15 @@ class BoilingDataset(Dataset):
         if len(self._filenames) == 0:
             return
         print(filename)
+        perm = (2, 0, 1)
         with h5py.File(filename, 'w') as f:
-            f.create_dataset('temperature', data=self._data['temp'])
-            f.create_dataset('velx', data=self._data['velx'])
-            f.create_dataset('vely', data=self._data['vely'])
-            f.create_dataset('dfun', data=self._data['dfun'])
-            f.create_dataset('pressure', data=self._data['pres'])
-            f.create_dataset('x', data=self._data['x'])
-            f.create_dataset('y', data=self._data['y'])
+            f.create_dataset('temperature', data=self._data['temp'].permute(perm))
+            f.create_dataset('velx', data=self._data['velx'].permute(perm))
+            f.create_dataset('vely', data=self._data['vely'].permute(perm))
+            f.create_dataset('dfun', data=self._data['dfun'].permute(perm))
+            f.create_dataset('pressure', data=self._data['pres'].permute(perm))
+            f.create_dataset('x', data=self._data['x'].permute(perm))
+            f.create_dataset('y', data=self._data['y'].permute(perm))
             
             REAL_RUNTIME_PARAMS = 'real runtime parameters'
             INT_RUNTIME_PARAMS = 'integer runtime parameters'
@@ -41,7 +42,7 @@ class BoilingDataset(Dataset):
                 f.create_dataset('int-runtime-params', data=f[INT_RUNTIME_PARAMS][:])
 
     def _load_data(self):
-        frame_dicts = self._load_files()
+        frame_dicts = self._load_files_par()
         var_dict = self._stack_frame_dicts(frame_dicts)
         return var_dict
 
@@ -64,46 +65,57 @@ class BoilingDataset(Dataset):
         self.xmin, self.xmax = frame0.xmin, frame0.xmax
         self.ymin, self.ymax = frame0.ymin, frame0.ymax
 
-    def _load_files(self):
-        frames = []
-        for filename in self._filenames:
-            frame = boxkit.read_dataset(filename, source='flash')
-            blocks = frame.blocklist
-            y_bs, x_bs = frame.nyb, frame.nxb
+    def _load_files_par(self):
+        NJOBS = 30
+        output = Parallel(n_jobs=NJOBS)(delayed(self._load_file)(idx, filename) for idx, filename in enumerate(self._filenames))
+        return output
 
-            blockx_pixel = x_bs * round(int((frame.xmax - frame.xmin)/blocks[0].dx)/x_bs)
-            blocky_pixel = y_bs * round(int((frame.ymax - frame.ymin)/blocks[0].dy)/y_bs)
-            nblockx = int(blockx_pixel/ x_bs)
-            nblocky = int(blocky_pixel/ y_bs)
+    def _load_file(self, idx, filename):
+        frame = boxkit.read_dataset(filename, source='flash')
+        blocks = frame.blocklist
+        y_bs, x_bs = frame.nyb, frame.nxb
 
-            nxb = nblockx * x_bs
-            nyb = nblocky * y_bs
+        blockx_pixel = x_bs * round(int((frame.xmax - frame.xmin)/blocks[0].dx)/x_bs)
+        blocky_pixel = y_bs * round(int((frame.ymax - frame.ymin)/blocks[0].dy)/y_bs)
+        nblockx = int(blockx_pixel/ x_bs)
+        nblocky = int(blocky_pixel/ y_bs)
 
-            var_dict = {}
-            for key in frame.varlist:
-                var_dict[key] = np.empty((nyb, nxb))
-                for block in blocks:
-                    r = y_bs * round(int((nyb * (block.ymin - frame.ymin))/(frame.ymax - frame.ymin))/y_bs)
-                    c = x_bs * round(int((nxb * (block.xmin - frame.xmin))/(frame.xmax - frame.xmin))/x_bs)
-                    var_dict[key][r:r+y_bs, c:c+x_bs] = block[key]
+        nxb = nblockx * x_bs
+        nyb = nblocky * y_bs
 
-            var_dict['x'] = np.empty((nyb, nxb))
-            var_dict['y'] = np.empty((nyb, nxb))
-            block_idx = 0
+        var_dict = {}
+        for key in frame.varlist:
+            var_dict[key] = np.empty((nyb, nxb))
             for block in blocks:
-                x, y = np.meshgrid(block.xrange('center'),
-                                   block.yrange('center'))
                 r = y_bs * round(int((nyb * (block.ymin - frame.ymin))/(frame.ymax - frame.ymin))/y_bs)
                 c = x_bs * round(int((nxb * (block.xmin - frame.xmin))/(frame.xmax - frame.xmin))/x_bs)
-                var_dict['x'][r:r+y_bs,c:c+x_bs] = x 
-                var_dict['y'][r:r+y_bs,c:c+x_bs] = y
+                var_dict[key][r:r+y_bs, c:c+x_bs] = block[key]
 
-            frames.append(var_dict)
-        return frames
+        var_dict['x'] = np.empty((nyb, nxb))
+        var_dict['y'] = np.empty((nyb, nxb))
+        block_idx = 0
+        for block in blocks:
+            x, y = np.meshgrid(block.xrange('center'),
+                               block.yrange('center'))
+            r = y_bs * round(int((nyb * (block.ymin - frame.ymin))/(frame.ymax - frame.ymin))/y_bs)
+            c = x_bs * round(int((nxb * (block.xmin - frame.xmin))/(frame.xmax - frame.xmin))/x_bs)
+            var_dict['x'][r:r+y_bs,c:c+x_bs] = x 
+            var_dict['y'][r:r+y_bs,c:c+x_bs] = y
+
+        return var_dict
+
+TWALL = 'Twall-'
 
 def unblock_dataset(write_dir, read_dir):
     b = BoilingDataset(read_dir)
-    dir_name = read_dir[read_dir.find('Twall-'):]
+
+    filename = Path(read_dir).stem
+    print(filename)
+    assert TWALL in filename, f'eek {TWALL} not in filename'
+    wall_temp = int(filename[len(TWALL):])
+    print(wall_temp)
+
+    dir_name = read_dir[read_dir.find(TWALL):]
     b.to_hdf5(f'{target}/{dir_name}.hdf5')
 
 if __name__ == '__main__':
@@ -112,9 +124,11 @@ if __name__ == '__main__':
 
     base = str(Path.home() / '/share/crsp/lab/ai4ts/share/simul_ts_0.1/SubCooled-FC72-2D/')
 
-    subdirs = [f for f in glob.glob(f'{base}/*') if 'Twall-' in f]
+    subdirs = [f for f in glob.glob(f'{base}/*') if TWALL in f]
     print(subdirs)
     
-    output = Parallel(n_jobs=len(subdirs))(delayed(unblock_dataset)(target, subdir) for subdir in subdirs)
-
+    for idx, subdir in enumerate(subdirs):
+        print(f'processing {subdir} {idx}/{len(subdirs)}')
+        unblock_dataset(target, subdir)
+    
     print('done!')
