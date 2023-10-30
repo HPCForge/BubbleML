8b7e235c97aeda44c29e0f41304023a4d697a793
Arthur Feeney <afeeney@uci.edu>
2023-10-29 20:39:03 -0700

    Remove src/ from git

--- src/models/pdearena/activations.py
+++
@@ -1,9 +0,0 @@
-from torch import nn
-
-ACTIVATION_REGISTRY = {
-    "relu": nn.ReLU(),
-    "silu": nn.SiLU(),
-    "gelu": nn.GELU(),
-    "tanh": nn.Tanh(),
-    "sigmoid": nn.Sigmoid(),
-}
--- src/op_lib/downsample.py
+++
@@ -1,7 +0,0 @@
-
-
-def downsample_domain(downsample_factor, *args):
-    if isinstance(downsample_factor, int):
-        downsample_factor = [downsample_factor, downsample_factor]
-    assert all([df >= 1 and isinstance(df, int) for df in downsample_factor])
-    return tuple([im[..., ::downsample_factor[0], ::downsample_factor[1]] for im in args])
