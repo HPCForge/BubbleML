ba902cd14be356bc15a69f8ca03eaae88452ecb6
HarshThomare <40192002+HarshThomare@users.noreply.github.com>
2024-03-02 18:08:17 -0800

    Transpose query_vector

--- sciml/models/trunk_wrapper/trunk_wrapper.py
+++ sciml/models/trunk_wrapper/trunk_wrapper.py
@@ -33,14 +33,12 @@ class TrunkWrapper(torch.nn.Module):
     def __init_module_list(self):
         module_list = torch.nn.ModuleDict()
         module_list["Branch"] = self.model
+        
         module_list['TrLinM1'] = torch.nn.Linear(1, self.width)
         module_list['TrActM1'] = torch.nn.ReLU()
         for i in range(2, self.trunk_depth):
             module_list['TrLinM{}'.format(i)] = torch.nn.Linear(self.width, self.width)
-            if (i == self.trunk_depth-1):
-                module_list['TrActM{}'.format(i)] = torch.nn.Softmax(dim=-1)
-            else:
-                module_list['TrActM{}'.format(i)] = torch.nn.ReLU()
+            module_list['TrActM{}'.format(i)] = torch.nn.ReLU()
 
         
         return module_list
@@ -63,7 +61,8 @@ class TrunkWrapper(torch.nn.Module):
             x_branch = torch.repeat_interleave(x_branch, self.fw, dim=0)
         else:
             x_branch = self.module_list["Branch"](x_branch)
-
+        
+        query_vector = torch.transpose(query_vector, 0, 1)
         for i in range(1, self.trunk_depth): 
             query_vector = self.module_list[f'TrLinM{i}'](query_vector)
             query_vector = self.module_list[f'TrActM{i}'](query_vector)
